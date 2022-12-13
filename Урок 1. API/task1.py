6f762823ac112976a106d0dd84bfdf7e9dc51fa6
owertone.learning <owertone.learning@gmail.com>
2022-12-13 15:21:15 +0300

    Задания к уроку 1
    Урок 1. Основы клиент-серверного взаимодействия. Работа с API
    
    Задания разделены на два файла.

---
+++ Урок 1. API/task1.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from pprint import pprint
+
+url = 'https://api.github.com/user'
+repo_url = 'https://api.github.com/user/repos'
+username = 'owertone'
+token = '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'
+
+#response = requests.get(url=url, auth=(username, token))
+#pprint(response.json())
+
+repos = requests.get(url=repo_url, auth=(username, token))
+print(repos.status_code)
+repo_dict = {}
+repo_dict['repositories'] = []
+for repo in repos.json():
+    if not repo['private']:
+        print(repo['full_name'], repo['html_url'])
+        repo_dict['repositories'].append({
+            'name': repo['full_name'],
+            'url': repo['html_url']
+        })
+with open('repositories.json', 'w', encoding='utf-8') as f:
+    json.dump(repo_dict, f, ensure_ascii=False, indent=2)
+print('Данные загружены!')
